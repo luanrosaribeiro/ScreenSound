d8ccd5256953023fde102f6287f2be11cf30341b
luanrosaribeiro <luanribeiro4445@gmail.com>
2024-08-09 12:07:37 -0300

    Primeiras alterações
    Iniciado a aplicação em console;
    Inicio das funções do CRUD.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # ScreenSound
-Projeto para treino da linguagem C#
+
+Bem-vindo ao repositório do **ScreenSound**! Este projeto tem como objetivo aprimorar meus conhecimentos em C# através do desenvolvimento de uma aplicação de música similar ao Spotify. Abaixo, você encontrará informações sobre o propósito do projeto, como configurá-lo e como acompanhar seu progresso.
+
+## Objetivo
+
+O **ScreenSound** é um projeto pessoal para praticar e expandir habilidades em C# ao criar uma aplicação de streaming de música. O objetivo é construir uma aplicação que emule as funcionalidades básicas de um serviço de música online, incluindo reprodução de faixas, gerenciamento de playlists e uma interface de usuário interativa.
+
+## Estrutura do Projeto
+
+O **ScreenSound** está dividido em várias seções que abordam diferentes aspectos da aplicação:
+
+- **Fundamentos do C#**: Estrutura do projeto, tipos de dados, controle de fluxo e outros conceitos básicos aplicados na construção da aplicação.
+- **Programação Orientada a Objetos (POO)**: Implementação de classes para representar usuários, faixas, playlists e outros elementos da aplicação.
+- **Interface Gráfica do Usuário (GUI)**: Desenvolvimento de uma interface de usuário interativa utilizando Windows Forms ou WPF (Windows Presentation Foundation).
+- **Manipulação de Arquivos e I/O**: Leitura e escrita de dados de faixas e playlists em arquivos ou banco de dados.
+- **Programação Assíncrona**: Gerenciamento de tarefas assíncronas para garantir uma experiência fluida e responsiva, como streaming de música e atualizações em tempo real.
---
+++ ScreenSound/ScreenSound/Program.cs
@@ -0,0 +1,91 @@
+﻿//Screen Sound
+
+List<string> listaDeBandas = new List<string>();
+
+void ExibirMensagemDeBoasVindas()
+{
+    Console.WriteLine(@"
+
+    ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+    ██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+    ╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+    ░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+    ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+    ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+    ");
+    string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+void ExibirOpcoesDoMenu()
+{
+    string opcaoEscolhida;
+    ExibirMensagemDeBoasVindas();
+    Console.WriteLine("---------------------MENU---------------------");
+    Console.WriteLine("1 - Registrar Banda");
+    Console.WriteLine("2 - Listar Bandas");
+    Console.WriteLine("3 - Avaliar Banda");
+    Console.WriteLine("4 - Exibir a média de avaliação de uma Banda");
+    Console.WriteLine("0 - Sair");
+    Console.Write("\nDigite a opção desejada: ");
+    opcaoEscolhida = Console.ReadLine()!;
+    switch (opcaoEscolhida)
+    {
+        case "1":
+            RegistrarBanda();
+            break;
+        case "2":
+
+            break;
+        case "3":
+            Console.WriteLine("Opção escolhida: " + opcaoEscolhida);
+            break;
+        case "4":
+            Console.WriteLine("Opção escolhida: " + opcaoEscolhida);
+            break;
+        case "0":
+            Console.WriteLine("Programa encerrado.");
+            break;
+        default:
+            Console.WriteLine("Opção inválida! Tente Novamente.");
+            ExibirOpcoesDoMenu();
+            break;
+
+    }
+}
+
+void RegistrarBanda()
+{
+    string nomeBanda;
+    Console.Clear();
+    Console.WriteLine("Resgistro de Banda");
+    Console.Write("Digite o nome da Banda: ");
+    nomeBanda = Console.ReadLine()!;
+    Cadastrar(listaDeBandas, nomeBanda);
+    Console.WriteLine("A banda {0} foi registrado com sucesso!", nomeBanda);
+    Thread.Sleep(2000);
+    ExibirOpcoesDoMenu();
+    Console.Clear();
+}
+
+void ListarBandas()
+{
+    Console.Clear();
+}
+
+
+void Cadastrar(List<string> lista, string valor)
+{
+    lista.Add(valor);
+}
+
+void Listar(List<string> lista)
+{
+    foreach (string valor in lista)
+    {
+
+    }
+}
+
+ExibirOpcoesDoMenu();
+
