02caadf378fed3e4a75507d73664caedee7aa5f1
luanrosaribeiro <luanribeiro4445@gmail.com>
2024-08-15 18:44:02 -0300

    Atualização na estrutura
    Troca de lista para dicionário;
    Ajustado menu de listar;
    Ajustado menu de cadastro;
    Adicionado cadastro de Avaliação;
    Adicionado funções de trechos de códigos que estavam repetitivo para otimizar o fonte.

--- ScreenSound/ScreenSound/Program.cs
+++ ScreenSound/ScreenSound/Program.cs
@@ -1,6 +1,9 @@
 ﻿//Screen Sound
 
-List<string> listaDeBandas = new List<string>();
+using System.Security;
+using System.Xml.Serialization;
+
+Dictionary<string,List<string>> dicionarioDeBandas = new Dictionary<string,List<string>>();
 
 void ExibirMensagemDeBoasVindas()
 {
@@ -13,15 +16,14 @@ void ExibirMensagemDeBoasVindas()
     ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
     ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
     ");
-    string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
-    Console.WriteLine(mensagemDeBoasVindas);
+    ExibeTituloMenus("Boas vindas ao Screen Sound");
 }
 
 void ExibirOpcoesDoMenu()
 {
     string opcaoEscolhida;
     ExibirMensagemDeBoasVindas();
-    Console.WriteLine("---------------------MENU---------------------");
+    ExibeTituloMenus("MENU");
     Console.WriteLine("1 - Registrar Banda");
     Console.WriteLine("2 - Listar Bandas");
     Console.WriteLine("3 - Avaliar Banda");
@@ -35,7 +37,7 @@ void ExibirOpcoesDoMenu()
             RegistrarBanda();
             break;
         case "2":
-
+            ListarBandas();
             break;
         case "3":
             Console.WriteLine("Opção escolhida: " + opcaoEscolhida);
@@ -54,39 +56,103 @@ void ExibirOpcoesDoMenu()
     }
 }
 
+void ExibeTituloMenus(string titulo)
+{
+    Console.Write(string.Empty.PadRight(titulo.Length, '-') + titulo + string.Empty.PadLeft(titulo.Length, '-'));
+    /*string separadores = string.Empty.PadLeft(titulo.Length, '-');
+    Console.WriteLine(separadores);
+    Console.WriteLine(titulo);
+    Console.WriteLine(separadores);*/
+}
+
 void RegistrarBanda()
 {
     string nomeBanda;
     Console.Clear();
-    Console.WriteLine("Resgistro de Banda");
+    ExibeTituloMenus("Resgistro de Banda");
     Console.Write("Digite o nome da Banda: ");
     nomeBanda = Console.ReadLine()!;
-    Cadastrar(listaDeBandas, nomeBanda);
+    Cadastrar(nomeBanda, null, dicionarioDeBandas);
     Console.WriteLine("A banda {0} foi registrado com sucesso!", nomeBanda);
     Thread.Sleep(2000);
-    ExibirOpcoesDoMenu();
     Console.Clear();
+    ExibirOpcoesDoMenu();
+}
+
+void RegistraAvaliacao()
+{
+    string avaliacao;
+    string banda;
+    Console.Clear();
+    ExibeTituloMenus("Avaliar Banda");
+    banda = VerificaBanda(dicionarioDeBandas);
+    avaliacao = Console.ReadLine()!;
+    Cadastrar(banda,avaliacao,dicionarioDeBandas);
+    Console.WriteLine("Avaliação registrada com sucesso!");
+    Thread.Sleep(2000);
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
+
+string VerificaBanda(Dictionary<string,List<string>> dicionario)
+{
+    Console.Write("Digite o nome da Banda que deseja avaliar: ");
+    string banda = Console.ReadLine()!;
+    if (dicionario.ContainsKey(banda))
+    {
+        return banda;
+    }
+    else
+    {
+        Console.WriteLine($"Banda: {banda} - Não encontrada em nossa base, tente novamente ou digite 0 para retornar ao menu principal.");
+        string valor = Console.ReadLine()!;
+        if (valor == "0")
+        {
+            ExibirOpcoesDoMenu();
+        }
+        else
+        {
+            VerificaBanda(dicionario);
+        }
+    }
+    return banda;
 }
 
 void ListarBandas()
 {
     Console.Clear();
-    Console.WriteLine("Lista das Bandas Cadastradas:");
-    Listar(listaDeBandas);
+    ExibeTituloMenus("Lista das Bandas Cadastradas");
+    Listar(dicionarioDeBandas);
+    RetornarMenu();
+}
+
+void RetornarMenu()
+{
     Console.WriteLine("Pressine qualquer tecla para voltar ao menu principal!");
     Console.ReadKey();
+    Console.Clear();
     ExibirOpcoesDoMenu();
+
+}
+
+void Cadastrar(string chave, string valor, Dictionary<string,List<string>> dicionario)
+{
+    if (chave != null && valor != null)
+    {
+        dicionario[chave].Add(valor);
+        return;
+    }
+    else if (chave != null && valor == null)
+    {
+        dicionario.Add(chave, new List<string>());
+        return;
+    }
 }
 
 
-void Cadastrar(List<string> lista, string valor)
+void Listar(Dictionary<string, List<string>> dicionario)
 {
-    lista.Add(valor);
-}
-
-void Listar(List<string> lista)
-{
-    foreach (string item in lista)
+    foreach (string item in dicionario.Keys)
     {
         Console.WriteLine($"->{item}");
     }
